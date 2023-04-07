42776f07b07a799b74adb66b2593241e66efea90
Astrid Liu <u7532120@anu.edu.au>
2023-04-08 00:34:06 +1000

    First attempt of the Login Page and jump interface

---
+++ app/src/main/java/com/example/ga_23s1_comp2100_6442/LoginPage.java
@@ -0,0 +1,46 @@
+package com.example.ga_23s1_comp2100_6442;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        TextView userName=(TextView) findViewById(R.id.userName);
+        TextView password=(TextView) findViewById(R.id.password);
+
+
+        MaterialButton loginBtn=(MaterialButton) findViewById(R.id.loginBtn);
+
+//        admin
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(userName.getText().toString().equals("admin")&&password.getText().toString().equals("admin")){
+//                    correct
+                    Toast.makeText(LoginPage.this,"LOGIN SUCCESSFUL",Toast.LENGTH_LONG).show();
+                    Intent intent=new Intent(LoginPage.this, HomePage.class);
+                    startActivity(intent);
+
+                }else{
+//                    incorrect
+                    Toast.makeText(LoginPage.this,"LOGIN FAIL!",Toast.LENGTH_LONG).show();
+
+
+                }
+            }
+        });
+    }
+}
