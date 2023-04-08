43596c76e2d0fd05c33464a75acce11c310710f9
Jinhao <u7531876@anu.edu.au>
2023-04-08 17:45:19 +1000

    Searching video using firebase

--- app/src/main/java/com/example/ga_23s1_comp2100_6442/HomePage.java
+++ app/src/main/java/com/example/ga_23s1_comp2100_6442/HomePage.java
@@ -6,18 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_page);
+
+        TextView videoName = (TextView) findViewById(R.id.vName);
         Button button = (Button) findViewById(R.id.buttonPlay);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String vn = videoName.getText().toString();
+                System.out.println("------------------------------------------------------");
+                System.out.println("vn:"+vn);
                 Intent intent = new Intent(HomePage.this, playVideo.class);
+                intent.putExtra("vn",vn);//transfer the vn to next activity
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/ga_23s1_comp2100_6442/playVideo.java
+++ app/src/main/java/com/example/ga_23s1_comp2100_6442/playVideo.java
@@ -1,5 +1,6 @@
 package com.example.ga_23s1_comp2100_6442;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
@@ -7,7 +8,14 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class playVideo extends AppCompatActivity {
     private VideoView mainVideoView;
@@ -17,16 +25,36 @@ public class playVideo extends AppCompatActivity {
     private ProgressBar currentProgress;
 
     private Uri videoUri;
-
+    private String vn;//video name
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_video);
-        mainVideoView=(VideoView) findViewById(R.id.videoView);
+        mainVideoView = (VideoView) findViewById(R.id.videoView);
 
-        videoUri= Uri.parse("https://firebasestorage.googleapis.com/v0/b/comp2100-comp6442-assignment.appspot.com/o/mixkit-pack-of-husky-dogs-barking-in-the-snow-48856-medium.mp4?alt=media&token=c52d9d27-4809-4746-a40f-718c6ab9c388");
-        mainVideoView.setVideoURI(videoUri);
+        Bundle bundle = getIntent().getExtras();//create bundle to get videoName;
+        System.out.println(bundle.getString("vn"));
+        if (bundle.getString("vn")!=null){
+            vn=bundle.getString("vn");
+        }
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference pathReference = storageRef.child(vn+".mp4");
+        pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                mainVideoView.setVideoURI(uri);
+                // Got the download URL for 'users/me/profile.png'
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                System.out.println("-----------------------------------------------------cannot find");
+                // Handle any errors
+            }
+        });
         mainVideoView.requestFocus();
         mainVideoView.start();
+
     }
 }
