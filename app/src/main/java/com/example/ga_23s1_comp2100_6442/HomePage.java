f5077a7f6880feb403ef6207c9e4ed039fc8e1f3
Jinhao <u7531876@anu.edu.au>
2023-04-08 16:00:24 +1000

    Test FireBase play video combine with astrid's homepage

--- app/src/main/java/com/example/ga_23s1_comp2100_6442/HomePage.java
+++ app/src/main/java/com/example/ga_23s1_comp2100_6442/HomePage.java
@@ -2,13 +2,24 @@ package com.example.ga_23s1_comp2100_6442;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.buttonPlay);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, playVideo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
