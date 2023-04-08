2c0edec16a96c85a42f9b3354627450572c39461
Jinhao <u7531876@anu.edu.au>
2023-04-08 23:24:11 +1000

    add process bar to the video

--- app/src/main/java/com/example/ga_23s1_comp2100_6442/playVideo.java
+++ app/src/main/java/com/example/ga_23s1_comp2100_6442/playVideo.java
@@ -3,9 +3,12 @@ package com.example.ga_23s1_comp2100_6442;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,22 +25,31 @@ public class playVideo extends AppCompatActivity {
     private ImageView playBtn;
     private TextView currentTimer;
     private TextView durationTimer;
+    private MediaController mediaController;
     private ProgressBar currentProgress;
 
     private String videoName;//video name for searching
+    private int playBackPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_video);
         mainVideoView = (VideoView) findViewById(R.id.videoView);
-
+        currentProgress = (ProgressBar) findViewById(R.id.progressBar);
         //create bundle to get videoName from last activity(HomePage)
         Bundle bundle = getIntent().getExtras();
         //check if videoName is passed by bundle
         if (bundle.getString("vn") != null) {
             videoName = bundle.getString("vn");
         }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
         //create firebase object
         FirebaseStorage storage = FirebaseStorage.getInstance();
         // Create a storage reference from our app
@@ -49,17 +61,49 @@ public class playVideo extends AppCompatActivity {
             public void onSuccess(Uri uri) {
                 // Got the download URL for 'videoName.mp4' and set to videoView
                 mainVideoView.setVideoURI(uri);
-
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception exception) {
-                Toast.makeText(playVideo.this,"Video not found!",Toast.LENGTH_LONG).show();
+                Toast.makeText(playVideo.this, "Video not found!", Toast.LENGTH_LONG).show();
                 // Handle any errors
             }
         });
-        mainVideoView.requestFocus();
-        mainVideoView.start();
 
+        currentProgress.setVisibility(View.VISIBLE);
+        mediaController = new MediaController(this);
+        mainVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mediaController.setAnchorView(findViewById(R.id.videoContainer));
+                mainVideoView.setMediaController(mediaController);
+                mainVideoView.seekTo(playBackPosition);
+                mainVideoView.start();
+            }
+        });
+        mainVideoView.setOnInfoListener(new MediaPlayer.OnInfoListener(){
+
+            @Override
+            public boolean onInfo(MediaPlayer mp, int what, int extra) {
+                if (what == MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START){
+                    currentProgress.setVisibility(View.INVISIBLE);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mainVideoView.pause();
+        playBackPosition = mainVideoView.getCurrentPosition();
+    }
+
+    @Override
+    protected void onStop() {
+        mainVideoView.stopPlayback();
+        super.onStop();
     }
 }
