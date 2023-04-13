15423ed9d6b96175cff1f334b5b2b24c8ea59909
Jinhao <u7531876@anu.edu.au>
2023-04-13 18:47:32 +1000

    user class

---
+++ app/src/main/java/com/example/ga_23s1_comp2100_6442/model/User.java
@@ -0,0 +1,28 @@
+package com.example.ga_23s1_comp2100_6442.model;
+
+public abstract class User {
+    String userName;
+    String name;
+    String institution;
+
+    public User() {
+    }
+
+    public User(String userName, String mame, String institution) {
+        this.userName = userName;
+        this.name = name;
+        this.institution = institution;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+}
