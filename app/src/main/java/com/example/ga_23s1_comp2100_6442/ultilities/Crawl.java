d41f45797cf810ab92dd29b24dfc99ce83adbe27
Rongyu Ma <u7530153@anu.edu.au>
2023-04-24 23:45:50 +1000

    Crawl

--- app/src/main/java/com/example/ga_23s1_comp2100_6442/ultilities/Crawl.java
+++ app/src/main/java/com/example/ga_23s1_comp2100_6442/ultilities/Crawl.java
@@ -1,28 +1,56 @@
 package com.example.ga_23s1_comp2100_6442.ultilities;
-
-import java.io.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.*;
-import java.net.*;
-
+import java.io.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Crawl {
-    public static void main(String[] args) {
-        String test_url = "https://www.coursera.org/search?page=1&index=prod_all_products_term_optimization_language_filter&topic=Computer%20Science";
-        try {
-            URL url = new URL(test_url);
-            URLConnection conn = url.openConnection();
-            InputStream is = conn.getInputStream();
-            System.out.println(conn.getContentEncoding());
-            BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
-            String line = null;
-            while((line=br.readLine())!=null){
-                System.out.println(line);
+    public static void main(String[] args) throws IOException {
+        String file_address = "D:\\eclipse-workspace\\Crawl1\\src\\main\\output\\crawl.csv";
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Meghann\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        for (int index = 1; index <= 84; index++) {
+            String test_url = String.format(
+                    "https://www.coursera.org/search?page=%d&index=prod_all_products_term_optimization_language_filter&topic=Information%%20Technology",
+                    index);
+            File csv = new File(file_address);
+            BufferedWriter bw = new BufferedWriter(new FileWriter(csv, true));
+
+            WebDriver driver = new ChromeDriver();
+            driver.manage()
+                    .timeouts()
+                    .implicitlyWait(Duration.ofSeconds(30));
+            driver.get(test_url);
+
+            List<WebElement> author = driver.findElements(By.cssSelector(".css-2fzscr"));
+            List<WebElement> title = driver.findElements(By.cssSelector(".css-bku0rr"));
+            List<WebElement> skills = driver.findElements(By.cssSelector(".css-5or6ht"));
+
+            if(author.size() != title.size() || author.size() != skills.size()){
+                bw.close();
+                driver.quit();
+                continue;
             }
-            br.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println(index);
+            System.out.println(author.size());
+            System.out.println(title.size());
+            System.out.println(skills.size());
+            for (int i = 0; i < author.size(); i++) {
+                String a = author.get(i).getText();
+                // System.out.println(a);
+                String t = title.get(i).getText();
+                // System.out.println(t);
+                String s = skills.get(i).getText().substring(9);
+                // System.out.println(s);
+                bw.write("\""+a+"\""+",");
+                bw.write("\""+t+"\""+",");
+                bw.write("\""+s+"\"");
+                bw.newLine();
+            }
+            bw.close();
+            driver.quit();
         }
     }
 }
