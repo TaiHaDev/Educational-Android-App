ebc90e15c50b1fb511f35664599b8dc2dcfc2b3b
Jinhao <u7531876@anu.edu.au>
2023-04-26 20:58:09 +1000

    user id in authentication match user id in direbase database when sign up

--- app/src/main/java/com/example/ga_23s1_comp2100_6442/SignUpPage.java
+++ app/src/main/java/com/example/ga_23s1_comp2100_6442/SignUpPage.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ga_23s1_comp2100_6442.model.Lecturer;
 import com.example.ga_23s1_comp2100_6442.model.Student;
@@ -20,6 +21,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Locale;
@@ -49,6 +51,10 @@ public class SignUpPage extends AppCompatActivity implements View.OnClickListene
         sw.setOnCheckedChangeListener(this);
         findViewById(R.id.signUpBtn).setOnClickListener(this);
         auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null) {
+            auth.signOut();
+        }
     }
 
 
@@ -60,11 +66,7 @@ public class SignUpPage extends AppCompatActivity implements View.OnClickListene
 //            //handle the already login user
 //        }
 //    }
-    private void registerUser() {
-        final String userNameS = userName.getText().toString().trim();
-        final String nameS = name.getText().toString().trim();
-        final String institutionS = institution.getText().toString().trim();
-        String passwordS = password.getText().toString().trim();
+    private void registerUser(String userNameS, String passwordS, String nameS, String institutionS) {
 
         System.out.println(userNameS);
         System.out.println(passwordS);
@@ -73,42 +75,52 @@ public class SignUpPage extends AppCompatActivity implements View.OnClickListene
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
                     // Sign in success, update UI with the signed-in user's information
+                    addUserToDatabase(userNameS, nameS, institutionS);
                     Log.d(TAG, "createUserWithEmail:success");
-//                    FirebaseUser user = mAuth.getCurrentUser();
-                    if (isLecture) {
-                        Lecturer lecturer = new Lecturer(
-                                userNameS,
-                                nameS,
-                                institutionS,
-                                Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid()
-                        );
-                        FirebaseFirestore db = FirebaseFirestore.getInstance();
-                        db.collection("lecturers").document(lecturer.getId()).set(lecturer);
-                    } else {
-                        Student student = new Student(
-                                userNameS,
-                                nameS,
-                                institutionS,
-                                Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid()
-                        );
-                        FirebaseFirestore db = FirebaseFirestore.getInstance();
-                        db.collection("students").document(student.getId()).set(student);
-                    }
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "createUserWithEmail:failure", task.getException());
                 }
             }
         });
-
-
     }
 
+    public void addUserToDatabase(String userNameS, String nameS, String institutionS) {
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //wait for current user to be initialize
+        while (user == null) {
+            user = auth.getCurrentUser();
+        }
+        if (isLecture) {
+            Lecturer lecturer = new Lecturer(
+                    userNameS,
+                    nameS,
+                    institutionS,
+                    user.getUid()
+            );
+            db.collection("lecturers").document(lecturer.getId()).set(lecturer);
+        } else {
+            Student student = new Student(
+                    userNameS,
+                    nameS,
+                    institutionS,
+                    user.getUid()
+            );
+            db.collection("students").document(student.getId()).set(student);
+        }
+    }
 
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.signUpBtn) {
-            registerUser();
+            final String userNameS = userName.getText().toString().trim();
+            final String nameS = name.getText().toString().trim();
+            final String institutionS = institution.getText().toString().trim();
+            String passwordS = password.getText().toString().trim();
+            registerUser(userNameS, passwordS, nameS, institutionS);
+            addUserToDatabase(userNameS, nameS, institutionS);
             Intent intent = new Intent(SignUpPage.this, LoginPage.class);
             startActivity(intent);
         }
